279a303f4506b0c63a5ffdd558c0b6a105322a75
Guillaume Fradin <guillaumefradin@gmail.com>
2023-12-01 16:52:12 +0000

    upgrade polygonjs to 1.5.45

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import {FileIFCSopNode} from './engine/nodes/sop/FileIFC';
 import {PolyPlugin} from '@polygonjs/polygonjs/dist/src/engine/poly/registers/plugins/Plugin';
 function PolygonjsPluginIFC(poly: PolyEngine) {
 	poly.registerOperation(FileIFCSopOperation);
-	console.log(FileIFCSopNode.type(), FileIFCSopNode.context());
 	poly.registerNode(FileIFCSopNode, CATEGORY_SOP.RENDER);
 }
 const polyPluginIFC = new PolyPlugin('ifc', PolygonjsPluginIFC, {
